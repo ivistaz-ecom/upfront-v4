bb24be61aa3e94c881b94653b7ad3e1be5670bf8
Shubham Shree <shubhamshree.dev@gmail.com>
2024-12-24 12:48:26 +0530

    video updated

--- components/HomePage/Banner.js
+++ components/HomePage/Banner.js
@@ -1,25 +1,23 @@
-import React from "react";
+import React from "react"
 
 const Banner = () => {
   return (
-    
-  <>
-  <div className="pointer-events-none py-0">
-  <video
-    src="https://s3.ap-south-1.amazonaws.com/com.cg.ivista.mediafiles/Upfront-v4/Upfront-Banner-Video.mp4"
-    width={1200}
-    height={900}
-    className="w-full h"
-    alt="home banner"
-    autoPlay
-    loop
-    muted
-    playsInline
-  />
-</div>
+    <>
+      <div className="pointer-events-none py-0">
+        <video
+          src="https://s3.ap-south-1.amazonaws.com/com.cg.ivista.mediafiles/Upfront-v4/Upfront+Banner-V12.mp4"
+          width={1200}
+          height={900}
+          className="w-full h"
+          alt="home banner"
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+      </div>
+    </>
+  )
+}
 
-  </>
-  );
-};
-
-export default Banner;
+export default Banner
